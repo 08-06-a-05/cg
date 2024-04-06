6de54fbebec8a0cc2284febac2bf09f648cb83be
MxxCon <78603662+08-06-a-05@users.noreply.github.com>
2024-04-06 17:30:11 +0300

    A lot of changes. Now drawing funcs dont count ladders, instead decorator does. Renamed class, now its name has sence. Added line type for better readbility, but may be it used not everywhere. Refactored graph builders funcs. Other small changes.

--- lab_03/main.py
+++ lab_03/main.py
@@ -2,7 +2,7 @@ import sys
 
 from PyQt6 import QtWidgets
 
-from new_interface import *
+from interface import *
 
 # TODO: Подписать оси
 
@@ -11,6 +11,5 @@ if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = QtWidgets.QMainWindow()
     ui = Ui_MainWindow(window)
-    ui.redraw_scene()
     window.show()
     app.exec()
